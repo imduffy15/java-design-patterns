968dd495edf0dd378467756da32843b1a2a6cdef
Ian Duffy <ian@ianduffy.ie>
2015-01-04 04:17:05 +0000

    add observer

---
+++ observer/src/main/java/ie/ianduffy/observer/WeatherObserver.java
@@ -0,0 +1,5 @@
+package ie.ianduffy.observer;
+
+public interface WeatherObserver {
+	void update(WeatherType currentWeather);
+}
---
+++ observer/src/main/java/ie/ianduffy/observer/WeatherType.java
@@ -0,0 +1,5 @@
+package ie.ianduffy.observer;
+
+public enum WeatherType {
+	COLD, RAINY, WINDY, SUNNY
+}
